42e996b939a4a851acb08343c6f46cbb76247d39
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-06 20:15:29 +0100

    Add type to projects

--- config/projects.php
+++ config/projects.php
@@ -5,24 +5,28 @@ return [
         'name' => 'QMK Firmware',
         'description' => 'This is a keyboard firmware based on the tmk_keyboard firmware with some useful features for Atmel AVR and ARM controllers, and more specifically, the OLKB product line, the ErgoDox EZ keyboard, and the Clueboard product line.',
         'url' => 'https://github.com/qmk/qmk_firmware',
-        'image' => 'https://cdn.icon-icons.com/icons2/3914/PNG/512/qmk_logo_icon_248727.png'
+        'image' => 'https://cdn.icon-icons.com/icons2/3914/PNG/512/qmk_logo_icon_248727.png',
+        'type_id' => 3
     ],
     [
         'name' => 'Home Assistant',
         'description' => 'Open source home automation that puts local control and privacy first. Powered by a worldwide community of tinkerers and DIY enthusiasts. Perfect to run on a Raspberry Pi or a local server.',
         'url' => 'https://github.com/home-assistant',
-        'image' => 'https://avatars.githubusercontent.com/u/13844975?s=200&v=4'
+        'image' => 'https://avatars.githubusercontent.com/u/13844975?s=200&v=4',
+        'type_id' => 1
     ],
     [
         'name' => 'Raspberry Pi',
         'description' => 'A collection of tools and resources for Raspberry Pi users.',
         'url' => 'https://github.com/raspberrypi',
-        'image' => 'https://avatars.githubusercontent.com/u/1294177?s=200&v=4'
+        'image' => 'https://avatars.githubusercontent.com/u/1294177?s=200&v=4',
+        'type_id' => 2
     ],
     [
         'name' => 'Arduino',
         'description' => 'A collection of tools and resources for Arduino users.',
         'url' => 'https://github.com/arduino',
-        'image' => 'https://avatars.githubusercontent.com/u/379109?s=200&v=4'
+        'image' => 'https://avatars.githubusercontent.com/u/379109?s=200&v=4',
+        'type_id' => 2
     ]
 ];
