ed9641b41604ef48652867c99a716fdf885c8175
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-07 13:13:02 +0100

    Style: change dashboard style with useful tools and informations

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,12 +21,45 @@
         </div>
     </div>
 
-    <div class="my-5">
-        @include('partials.library')
-    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card w-100 m-2 glass" style="width: 18rem;">
+                <div class="card-img-top-container dash d-flex align-items-center justify-content-center">
+                </div>
+                <div class="card-body">
+                    <h1 class="card-title text-center">Projects
+                    </h1>
+                    <p class="card-text text-center">There are {{ count($projects) }} projects</p>
+                </div>
+                <div class="d-flex align-items-center mb-3 ps-3 justify-self-end justify-content-center">
+                    <a class="btn btn-primary me-3 add-cart-btn w-100 d-flex justify-content-center align-items-center"
+                        href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list-ul fs-2"></i>
+                    </a>
+                    <a class="btn btn-primary me-3 add-cart-btn w-100 d-flex justify-content-center align-items-center"
+                        href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-plus fs-2"></i>
+                    </a>
+                </div>
+            </div>
 
-    <a id="add-project" class="glass border-0 text-black btn btn-primary d-flex align-items-center justify-content-center"
-        href="{{ route('admin.projects.create') }}">
-        <i class="fa-solid fa-plus fs-3"></i>
-    </a>
+            <div class="card w-100 m-2 glass" style="width: 18rem;">
+                <div class="card-img-top-container dash d-flex align-items-center justify-content-center">
+                </div>
+                <div class="card-body">
+                    <h1 class="card-title text-center">Types
+                    </h1>
+                    <p class="card-text text-center">There are {{ count($types) }} project types</p>
+                </div>
+                <div class="d-flex align-items-center mb-3 ps-3 justify-self-end justify-content-center">
+                    <a class="btn btn-primary me-3 add-cart-btn w-100 d-flex justify-content-center align-items-center"
+                        href="{{ route('admin.types.index') }}"><i class="fa-solid fa-list-ul fs-2"></i>
+                    </a>
+                    <a class="btn btn-primary me-3 add-cart-btn w-100 d-flex justify-content-center align-items-center"
+                        href="{{ route('admin.types.create') }}"><i class="fa-solid fa-plus fs-2"></i>
+                    </a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 @endsection
